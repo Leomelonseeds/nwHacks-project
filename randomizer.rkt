92e614da54085c96fdb98bcce38f049ffb28565b
Leomelonseeds <leonardfang15@gmail.com>
2022-01-15 15:29:26 -0800

    Add head and few examples

--- randomizer.rkt
+++ randomizer.rkt
@@ -1,4 +1,18 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname randomizer) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #t)))
-"test"
+(require 2htdp/image)
+(require 2htdp/universe)
+
+;; Head, body, arms, legs
+
+;; Head is
+(define-struct head (image chance))
+;; image is Image ; head image
+;; chance is Natural ; rarity, where higher numbers are more common
+
+;; ListOfHead is (listof Head)
+
+(define head1 (make-head (circle 20 "solid" "blue") 5))
+(define head2 (make-head (rectangle 20 10 "solid" "green") 6))
+
