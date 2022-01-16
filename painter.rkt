95a63eb8a444529fefe08d411590536ca765c91c
Leomelonseeds <leonardfang15@gmail.com>
2022-01-15 17:19:32 -0800

    Added painter.rkt and painter.bak

---
+++ painter.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname painter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #t)))
+(require 2htdp/image)
+(require 2htdp/universe)
+
+;; A program allowing users to paint their own body parts
